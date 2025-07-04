08be1c5598d7d2b5904d518ed0a256a80790e0a6
osmonalieva-saule <osmonalievasaule1@gmail.com>
2025-07-04 14:10:02 +0600

    4 lesson

---
+++ app/src/main/java/com/example/hw_3_6month/data/dto/ResponseCharacter.kt
@@ -0,0 +1,7 @@
+package com.example.hw_3_6month.data.dto
+
+import com.example.hw_3_6month.data.dto.character.CharacterDTO
+
+data class ResponseCharacter(
+    val results: List<CharacterDTO> ? = null
+)
